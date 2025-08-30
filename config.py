4506e31058feb72fda6f41c8320417c705621fec
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-08-31 04:56:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 class CBOT:
-    API_ID = 
-    API_HASH = ""
+    API_ID = 28932643
+    API_HASH = "3fb38bb53a7d33fb42ea208c6f77830b"
     BOT_TOKEN = ""
-    LOG_FILE_NAME = ""
-    LOG_GROUP_ID = 
-    BANNER_IMG_URL = ""
-    SUPPORT_GROUP = ""
-    UPDATE_CHANNEL = ""
-    BOT_USERNAME = ""
+    LOG_FILE_NAME = "nkbot"
+    LOG_GROUP_ID = -1002828344230
+    BANNER_IMG_URL = "https://files.catbox.moe/k5o3lc.jpg"
+    SUPPORT_GROUP = "NakamaSuport"
+    UPDATE_CHANNEL = "NakamaMarket"
+    BOT_USERNAME = "Nakamaprotectbot"
 class COWNER:
-    OWNER_USERNAME = ""
-    OWNER_ID = 
+    OWNER_USERNAME = "Nakamahire"
+    OWNER_ID = 6305402536
 class CDATABASE:
-    DATABASE_URL = ""
+    DATABASE_URL = "mongodb+srv://dizaubot:dizaubot@cluster0.ise8rzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-DEV = ""
+DEV = "6305402536"
