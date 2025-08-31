d6bc1ca4aa221d3844dc1d7a3e209db56f429959
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-08-31 08:28:05 +0800

    Update message.py

--- main/commands/message.py
+++ main/commands/message.py
@@ -1,3 +1,5 @@
+import json
+import os
 from main.helpers.utils.gibberish import gibberish
 from main.helpers.utils.notification import notification
 from main.database import groupdb, blockwordsdb, userdb
@@ -5,10 +7,30 @@ from main.helpers.utils.similiarity import AdvancedSimilarityChecker
 
 similarity_checker = AdvancedSimilarityChecker(threshold=0.7)
 
+# Load daftar blacklist dari bl.json
+def load_blacklist():
+    if os.path.exists("bl.json"):
+        with open("bl.json", "r") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+    return []
+
 
 async def message_func(client, message):
     chat_id = message.chat.id
-    user_id = message.from_user.id
+    user_id = message.from_user.id if message.from_user else None
+    if not user_id:
+        return
+
+    # cek global blacklist
+    blacklist = load_blacklist()
+    if str(user_id) in [str(uid) for uid in blacklist]:
+        await message.delete()
+        await userdb.remove_user(user_id)  # hapus dari db user
+        return
+
     state_group = await groupdb.get_antibc_state(chat_id)
     blockwords = await blockwordsdb.get_blockwords(chat_id)
     whitelistuser = await userdb.is_whitelisted(chat_id, user_id)
