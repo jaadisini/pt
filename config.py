7615f51d7365c6c968434db315c6ac219567aa03
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-12-27 22:35:44 +0800

    Update LOG_GROUP_ID in config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class CBOT:
     API_HASH = "3fb38bb53a7d33fb42ea208c6f77830b"
     BOT_TOKEN = "8047031862:AAGe6MiNReCzybi-QKAoVrF-XwChJ8XdoZ0"
     LOG_FILE_NAME = "nkbot"
-    LOG_GROUP_ID = -1002828344230
+    LOG_GROUP_ID = -1002729458912
     BANNER_IMG_URL = "https://files.catbox.moe/k5o3lc.jpg"
     SUPPORT_GROUP = "NakamaSuport"
     UPDATE_CHANNEL = "NakamaMarket"
