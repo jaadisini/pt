b5bdcda23700787931492a62e87d11b87e2e8b41
jaadisini <162589971+jaadisini@users.noreply.github.com>
2026-03-01 10:25:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class CBOT:
     LOG_FILE_NAME = "nkbot"
     LOG_GROUP_ID = -1002729458912
     BANNER_IMG_URL = "https://files.catbox.moe/k5o3lc.jpg"
-    SUPPORT_GROUP = "NakamaSuport"
-    UPDATE_CHANNEL = "NakamaMarket"
+    SUPPORT_GROUP = "NakamaHire"
+    UPDATE_CHANNEL = "NakamaHire"
     BOT_USERNAME = "Nakamaprotectbot"
 class COWNER:
-    OWNER_USERNAME = "Nakamahire"
-    OWNER_ID = 6305402536
+    OWNER_USERNAME = "NakamaHire"
+    OWNER_ID = 1816904396
 class CDATABASE:
     DATABASE_URL = "mongodb+srv://karangasem:1234@cluster0.34crnft.mongodb.net/?appName=Cluster0"
 
-DEV = "6305402536"
+DEV = "1816904396"
