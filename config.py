e251f95cab3ae75aa5e629100c76e832a3fc7c69
jaadisini <162589971+jaadisini@users.noreply.github.com>
2026-08-17 08:15:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class CBOT:
     API_ID = 28932643
     API_HASH = "3fb38bb53a7d33fb42ea208c6f77830b"
-    BOT_TOKEN = "8047031862:AAGe6MiNReCzybi-QKAoVrF-XwChJ8XdoZ0"
+    BOT_TOKEN = "8615408270:AAHiGN6Wf9DHpv2I5WQN0AujmuQurWn22pw"
     LOG_FILE_NAME = "nkbot"
     LOG_GROUP_ID = -1002729458912
     BANNER_IMG_URL = "https://files.catbox.moe/k5o3lc.jpg"
@@ -10,8 +10,8 @@ class CBOT:
     BOT_USERNAME = "Nakamaprotectbot"
 class COWNER:
     OWNER_USERNAME = "NakamaHire"
-    OWNER_ID = 1816904396
+    OWNER_ID = 1226169879
 class CDATABASE:
     DATABASE_URL = "mongodb+srv://karangasem:1234@cluster0.34crnft.mongodb.net/?appName=Cluster0"
 
-DEV = "1816904396"
+DEV = "1226169879"
